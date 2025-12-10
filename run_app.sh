ff8cc073bc3376920181b7c459a32ba1dfa84958
Anthony Enrique Velasquez Honorio <anthony2000_honorio@outlook.com>
2025-12-09 19:41:57 -0500

    update

--- run_app.sh
+++ run_app.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 
-# Ruta a tu aplicación principal FastAPI
 APP_MODULE="main:app"
 
-
-# Nombre del entorno virtual (ajusta si usas otro)
-VENV_PATH="virtual/Scripts/activate"
-
-# Activar entorno virtual (si lo estás usando)
-if [ -f "$VENV_PATH" ]; then
-    source "$VENV_PATH"
-fi
-
-
-# Iniciar el servidor FastAPI con Uvicorn
 echo "Iniciando servidor FastAPI..."
-uvicorn $APP_MODULE --reload
-
+python -m uvicorn $APP_MODULE --host 0.0.0.0 --port 8000
